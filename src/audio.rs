0728402a140d45c26741fc4b5811343300cc1fb6
Jason Reed <jcreed@gmail.com>
2024-04-19 19:36:52 -0400

    put in stub for note rendering

--- src/audio.rs
+++ src/audio.rs
@@ -12,6 +12,24 @@ const TABLE_SIZE: usize = 4000;
 
 impl AudioService {
   pub fn new(data: &Data) -> Mostly<AudioService> {
+    // Initialise wavetable.
+    let mut wavetable = vec![0.0; TABLE_SIZE + 1];
+    for i in 0..TABLE_SIZE {
+      // // SINE
+      // wavetable[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0).sin() as f32;
+
+      // // SQUARE
+      // wavetable[i] = if (i as f64 / TABLE_SIZE as f64) < 0.5 {
+      //   -1.0
+      // } else {
+      //   1.0
+      // };
+
+      //  SAW
+      wavetable[i] = (2.0 * (i as f64 / TABLE_SIZE as f64) - 1.0) as f32;
+    }
+    wavetable[TABLE_SIZE] = wavetable[0];
+
     let pa = pad::PortAudio::new()?;
 
     let mut settings =
@@ -21,7 +39,7 @@ impl AudioService {
 
     let sg = data.state.clone();
     let sg2 = data.state.clone();
-    let lowp_len: usize = 30;
+    let lowp_len: usize = 5;
     let mut lowp: Vec<f32> = vec![0.0; lowp_len];
     let mut lowp_ix = 0;
 
@@ -33,13 +51,13 @@ impl AudioService {
       let mut s: MutexGuard<State> = sg.lock().unwrap();
       for ix in 0..frames {
         let mut samp = 0.0;
-        s.phase += s.freq / SAMPLE_RATE;
-        if s.phase > 1. {
-          s.phase -= 1.;
+
+        for mut note in s.note_state.iter_mut() {
+          exec_note(&mut note, &wavetable, &mut samp);
         }
-        let out: f32 = 0.01 * wave(s.phase);
-        buffer[2 * ix] = out;
-        buffer[2 * ix + 1] = out;
+
+        buffer[2 * ix] = samp;
+        buffer[2 * ix + 1] = samp;
       }
 
       if s.going {
@@ -65,4 +83,77 @@ impl AudioService {
     stream.close()?;
     Ok(AudioService {})
   }
+}
+
+const ATTACK: f32 = 0.005; // seconds
+const DECAY: f32 = 0.005; // seconds
+const SUSTAIN: f32 = 0.5; // dimensionless
+const RELEASE: f32 = 0.15; // seconds
+
+pub fn note_fsm_amp(fsm: &NoteFsm) -> f32 {
+  match *fsm {
+    NoteFsm::On { t, amp, vel } => {
+      if t < ATTACK {
+        let a = t / ATTACK;
+        amp * (1.0 - a) + vel * a
+      } else if t < ATTACK + DECAY {
+        let a = (t - ATTACK) / DECAY;
+        vel * (1.0 - a) + vel * SUSTAIN * a
+      } else {
+        SUSTAIN * vel
+      }
+    }
+    NoteFsm::Release { t, amp } => amp * (1.0 - (t / RELEASE)),
+  }
+}
+
+fn advance_note(note: &mut Option<NoteState>) {
+  match note {
+    Some(NoteState {
+      fsm: NoteFsm::On { ref mut t, .. },
+      ..
+    }) => {
+      *t += 1.0 / SAMPLE_RATE;
+    }
+    Some(NoteState {
+      fsm: NoteFsm::Release { ref mut t, .. },
+      ..
+    }) => {
+      *t += 1.0 / SAMPLE_RATE;
+      if *t > RELEASE {
+        *note = None;
+      }
+    }
+    None => (),
+  }
+}
+
+fn wrap_not_mod<T: std::cmp::PartialOrd + std::ops::SubAssign + std::convert::From<f32>>(
+  x: &mut T,
+  size: T,
+) {
+  if *x >= size {
+    *x = 0.0.into();
+  }
+}
+
+fn exec_note(onote: &mut Option<NoteState>, wavetable: &[f32], samp: &mut f32) {
+  match *onote {
+    None => (),
+    Some(ref mut note) => {
+      let phase: f32 = note.phase;
+      let offset = note.phase.floor() as usize;
+      let fpart: f32 = (phase as f32) - (offset as f32);
+
+      // linear interp
+      let table_val = fpart * wavetable[offset + 1] + (1.0 - fpart) * wavetable[offset];
+
+      let scale = note_fsm_amp(&note.fsm);
+      *samp += (scale as f32) * table_val;
+      let base = note.freq * (TABLE_SIZE as f32) / SAMPLE_RATE;
+      note.phase += base;
+      wrap_not_mod(&mut note.phase, TABLE_SIZE as f32);
+    }
+  }
+  advance_note(onote);
 }
