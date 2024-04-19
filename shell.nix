752650fcbe2c409dd462d29ce4fd935cc0375d5a
Jason Reed <jcreed@gmail.com>
2024-04-19 12:08:28 -0400

    Don't need to patch

--- shell.nix
+++ shell.nix
@@ -3,18 +3,12 @@
     asoundShellHook = ''
     export ALSA_PLUGIN_DIR=${alsa-plugins}/lib/alsa-lib
     '';
-
-    patchedAlsa = trace alsa-lib.patches
-      alsa-lib.overrideAttrs (finala: preva: {
-      patches = preva.patches ++ [ ./patches/0001-Debug-a-little.patch ];
-    });
   in
     mkShell {
       nativeBuildInputs = [
         rustc
         rust-analyzer
-        # alsa-lib.dev
-        patchedAlsa.dev
+        alsa-lib.dev
         alsa-plugins
       ];
       shellHook = asoundShellHook;
