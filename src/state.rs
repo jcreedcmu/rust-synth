2cd279ed0fff300df95cdce1cf8a4b14570940a1
Jason Reed <jcreed@gmail.com>
2024-04-22 12:15:36 -0400

    Simplistic bass drum synth

--- src/state.rs
+++ src/state.rs
@@ -20,6 +20,7 @@ pub enum EnvState {
 #[derive(Clone, Debug)]
 pub struct BassDrumSynthState {
   pub t_s: f32,
+  pub phase: f32,
 }
 
 #[derive(Clone, Debug)]
--- src/synth.rs
+++ src/synth.rs
@@ -1,38 +1,66 @@
+use rand::Rng;
+
 use crate::consts::SAMPLE_RATE_hz;
 use crate::state::{BassDrumSynthState, EnvState, ReasonableSynthState, UgenState};
 
 pub const TABLE_SIZE: usize = 4000;
 
 pub struct Synth {
-  wavetable: Vec<f32>,
+  saw_wavetable: Vec<f32>,
+  noise_wavetable: Vec<f32>,
 }
 
 impl Synth {
   pub fn new() -> Synth {
     // Initialise wavetable.
-    let mut wavetable = vec![0.0; TABLE_SIZE + 1];
+    let mut saw_wavetable = vec![0.0; TABLE_SIZE + 1];
+    let mut noise_wavetable = vec![0.0; TABLE_SIZE + 1];
+
+    // // SINE
+    // wavetable[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0).sin() as f32;
+
+    // // SQUARE
+    // wavetable[i] = if (i as f64 / TABLE_SIZE as f64) < 0.5 {
+    //   -1.0
+    // } else {
+    //   1.0
+    // };
+
+    // Why did we make TABLE_SIZE + 1 with this wraparound? It seems I
+    // originally did it so that we can do linear interpolation
+    // without worrying about %. Not clear if this really matters for
+    // performance.
     for i in 0..TABLE_SIZE {
-      // // SINE
-      // wavetable[i] = (i as f64 / TABLE_SIZE as f64 * PI * 2.0).sin() as f32;
-
-      // // SQUARE
-      // wavetable[i] = if (i as f64 / TABLE_SIZE as f64) < 0.5 {
-      //   -1.0
-      // } else {
-      //   1.0
-      // };
-
-      //  SAW
-      wavetable[i] = (2.0 * (i as f64 / TABLE_SIZE as f64) - 1.0) as f32;
+      saw_wavetable[i] = (2.0 * (i as f64 / TABLE_SIZE as f64) - 1.0) as f32;
     }
-    wavetable[TABLE_SIZE] = wavetable[0];
+    saw_wavetable[TABLE_SIZE] = saw_wavetable[0];
 
-    Synth { wavetable }
+    for i in 0..TABLE_SIZE {
+      noise_wavetable[i] = rand::thread_rng().gen_range(-1.0f32..1.0f32);
+    }
+    noise_wavetable[TABLE_SIZE] = noise_wavetable[0];
+
+    Synth {
+      saw_wavetable,
+      noise_wavetable,
+    }
   }
 
   // FIXME: do the dispatch with traits or something
   fn exec_bass_synth(self: &Synth, state: &mut BassDrumSynthState, samp: &mut f32) {
-    // let table_val = fpart * self.wavetable[offset + 1] + (1.0 - fpart) * self.wavetable[offset];
+    let phase: f32 = state.phase;
+    let offset = state.phase.floor() as usize;
+    let fpart: f32 = (phase as f32) - (offset as f32);
+    let table_val =
+      fpart * self.noise_wavetable[offset + 1] + (1.0 - fpart) * self.noise_wavetable[offset];
+
+    const bass_drum_freq_hz: f32 = 20.0;
+    let base = bass_drum_freq_hz * (TABLE_SIZE as f32) / SAMPLE_RATE_hz;
+
+    // XXX This phase alteration should be in advance, not in exec, I think. Probably
+    // exec should take a non-mutable reference to ugen state.
+    state.phase += base;
+    wrap_not_mod(&mut state.phase, TABLE_SIZE as f32);
   }
 
   fn exec_reasonable_synth(self: &Synth, state: &mut ReasonableSynthState, samp: &mut f32) {
@@ -41,11 +69,15 @@ impl Synth {
     let fpart: f32 = (phase as f32) - (offset as f32);
 
     // linear interp
-    let table_val = fpart * self.wavetable[offset + 1] + (1.0 - fpart) * self.wavetable[offset];
+    let table_val =
+      fpart * self.saw_wavetable[offset + 1] + (1.0 - fpart) * self.saw_wavetable[offset];
 
     let scale = ugen_env_amp(&state.env_state);
     *samp += (scale as f32) * table_val;
     let base = state.freq_hz * (TABLE_SIZE as f32) / SAMPLE_RATE_hz;
+
+    // XXX This phase alteration should be in advance, not in exec, I think. Probably
+    // exec should take a non-mutable reference to ugen state.
     state.phase += base;
     wrap_not_mod(&mut state.phase, TABLE_SIZE as f32);
   }
@@ -107,8 +139,12 @@ fn advance_ugen(ugen: &mut Option<UgenState>) {
         *ugen = None;
       }
     },
-    Some(UgenState::BassDrumSynth(BassDrumSynthState { ref mut t_s })) => {
+    Some(UgenState::BassDrumSynth(BassDrumSynthState { ref mut t_s, .. })) => {
       *t_s += tick_s;
+      const BASS_DRUM_DEBUG_RELEASE_s: f32 = 0.2;
+      if *t_s > BASS_DRUM_DEBUG_RELEASE_s {
+        *ugen = None;
+      }
     },
     None => (),
   }
