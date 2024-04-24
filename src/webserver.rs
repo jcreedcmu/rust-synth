04f0d813b04924c516d0e502c20c15ba498ba74b
Jason Reed <jcreed@gmail.com>
2024-04-24 14:14:37 -0400

    Sketch in websocket code

--- src/webserver.rs
+++ src/webserver.rs
@@ -1,4 +1,6 @@
-use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use actix::StreamHandler;
+use actix_web::{post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
 use serde::Deserialize;
 use tokio::sync::mpsc::{channel, Sender};
 
@@ -26,11 +28,44 @@ async fn action(
   HttpResponse::Ok().body("{}")
 }
 
+struct MyWs;
+
+impl actix::Actor for MyWs {
+  type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+  fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+    match msg {
+      Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+      Ok(ws::Message::Text(text)) => {
+        println!("### text {}", text);
+      },
+      Ok(ws::Message::Binary(bin)) => {
+        println!("### binary");
+        ctx.binary(bin);
+      },
+      _ => (),
+    }
+  }
+}
+
+async fn ws_index(
+  req: HttpRequest,
+  stream: web::Payload,
+  tx: actix_web::web::Data<Sender<WebMessage>>,
+) -> Result<HttpResponse, actix_web::Error> {
+  let resp = ws::start(MyWs {}, &req, stream);
+  println!("{:?}", resp);
+  resp
+}
+
 #[actix_web::main]
 pub async fn serve(tx: Sender<WebMessage>) -> std::io::Result<()> {
   HttpServer::new(move || {
     App::new()
       .app_data(actix_web::web::Data::new(tx.clone()))
+      .route("/ws/", web::get().to(ws_index))
       .service(action)
       .service(actix_files::Files::new("/", "./public").index_file("index.html"))
   })
