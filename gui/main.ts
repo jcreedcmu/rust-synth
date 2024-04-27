e5f64d555e34065adc6756473139ef5d1a2a9a18
Jason Reed <jcreed@gmail.com>
2024-04-27 15:34:26 -0400

    Restart websocket

--- gui/main.ts
+++ gui/main.ts
@@ -3,17 +3,37 @@ import { WebMessage } from "./protocol";
 
 function go() {
 
-  const ws = new WebSocket('/ws/')
-  ws.onopen = () => {
+  let wsc: { ws: WebSocket } = { ws: new WebSocket('/ws/') };
+
+  wsc.ws.onopen = () => {
     console.log('ws opened on browser')
   }
 
-  ws.onmessage = message => {
+  wsc.ws.onclose = () => {
+    console.log('ws closed on browser')
+    setTimeout((() => {
+      reconnect(wsc);
+    }), 1500);
+  }
+
+  wsc.ws.onmessage = message => {
     console.log(`message received`, message.data);
   }
 
+  function reconnect(wsc: { ws: WebSocket }) {
+    console.log('retrying...')
+    let ws = wsc.ws;
+    var ws2 = new WebSocket(ws.url);
+    ws2.onopen = ws.onopen;
+    ws2.onmessage = ws.onmessage;
+    ws2.onclose = ws.onclose;
+    ws2.onerror = ws.onerror;
+    wsc.ws = ws2;
+  }
+
+
   function send(sm: WebMessage) {
-    ws.send(JSON.stringify(sm));
+    wsc.ws.send(JSON.stringify(sm));
   }
 
   const action = document.getElementById('action')!;
