ec7899553e85e3385ba3ece429526c0e539269e8
Jason Reed <jcreed@gmail.com>
2024-04-27 15:53:23 -0400

    Better reconnection logic

--- gui/app.tsx
+++ gui/app.tsx
@@ -13,36 +13,39 @@ export function init(props: AppProps) {
 type WebSocketContainer = { ws: WebSocket };
 
 function App(props: AppProps): JSX.Element {
+  const [connected, setConnected] = useState(true);
+  const [drumVolume, setDrumVolume] = useState(100);
   const wsco = useRef<WebSocketContainer | undefined>(undefined);
+
+  function reconnect(wsc: WebSocketContainer) {
+    console.log('retrying...')
+    let ws = wsc.ws;
+    var ws2 = new WebSocket(ws.url);
+    ws2.onopen = ws.onopen;
+    ws2.onmessage = ws.onmessage;
+    ws2.onclose = ws.onclose;
+    ws2.onerror = ws.onerror;
+    wsc.ws = ws2;
+  }
+
   useEffect(() => {
     let wsc: WebSocketContainer = { ws: new WebSocket('/ws/') };
 
     wsc.ws.onopen = () => {
+      setConnected(true);
       console.log('ws opened on browser')
       wsco.current = wsc;
     }
 
     wsc.ws.onclose = () => {
+      setConnected(false);
       console.log('ws closed on browser')
-      setTimeout((() => {
-        reconnect(wsc);
-      }), 1500);
     }
 
     wsc.ws.onmessage = message => {
       console.log(`message received`, message.data);
     }
 
-    function reconnect(wsc: { ws: WebSocket }) {
-      console.log('retrying...')
-      let ws = wsc.ws;
-      var ws2 = new WebSocket(ws.url);
-      ws2.onopen = ws.onopen;
-      ws2.onmessage = ws.onmessage;
-      ws2.onclose = ws.onclose;
-      ws2.onerror = ws.onerror;
-      wsc.ws = ws2;
-    }
   }, []);
 
   function send(sm: WebMessage) {
@@ -55,13 +58,16 @@ function App(props: AppProps): JSX.Element {
   }
 
   const onInput = (e: Event) => {
-    send({ message: { t: 'setVolume', vol: parseInt((e.target as HTMLInputElement).value) } });
+    const vol = parseInt((e.target as HTMLInputElement).value);
+    send({ message: { t: 'setVolume', vol } });
+    setDrumVolume(vol);
   };
-  const connected = false;
+
   return <div>
-    <button onMouseDown={() => { send({ message: { t: 'drum' } }) }}>Action</button><br />
-    <button onMouseDown={() => { send({ message: { t: 'quit' } }) }}>Quit</button><br />
-    <input type="range" min="0" max="100" value="100" onInput={onInput} />
-    {!connected ? <span style={{ backgroundColor: 'red', color: 'white' }}>DISCONNECTED<br /></span> : undefined}
+    <button disabled={!connected} onMouseDown={() => { send({ message: { t: 'drum' } }) }}>Action</button><br />
+    <button disabled={!connected} onMouseDown={() => { send({ message: { t: 'quit' } }) }}>Quit</button><br />
+    <input disabled={!connected} type="range" min="0" max="100" value={drumVolume} onInput={onInput} />
+    {!connected ? <span><br /><button style={{ backgroundColor: 'red', color: 'white' }}
+      onClick={() => { reconnect(wsco.current!); }}>reconnect</button></span> : undefined}
   </div>;
 }
