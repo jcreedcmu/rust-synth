2642a6733651df9049970d906745aa177fc26a32
Jason Reed <jcreed@gmail.com>
2024-05-11 12:47:07 -0400

    Can fiddle around with taps interactively

--- gui/app.tsx
+++ gui/app.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import { CSSProperties, useEffect, useRef, useState } from 'react';
-import { WebMessage } from './protocol';
+import { LowpassControlBlock, WebMessage } from './protocol';
 import { produce } from 'immer';
 import { Chart } from './chart';
 
@@ -54,10 +54,40 @@ function Sequencer(props: SequencerProps): JSX.Element {
   return <table>{rows}</table>;
 }
 
+type LowpassCfgProps = {
+  cfg: LowpassControlBlock,
+  setLowpassCfg: (x: LowpassControlBlock) => void,
+}
+
+function LowpassCfg(props: LowpassCfgProps): JSX.Element[] {
+
+  const setPos = (e: React.FormEvent, ix: number) => {
+    const pos = parseInt((e.target as HTMLInputElement).value);
+    const newCfg = produce(props.cfg, cfg => { cfg.taps[ix].pos = pos; });
+    props.setLowpassCfg(newCfg);
+  };
+
+  const setWeight = (e: React.FormEvent, ix: number) => {
+    const weight = parseInt((e.target as HTMLInputElement).value);
+    const newCfg = produce(props.cfg, cfg => { cfg.taps[ix].weight = weight / 100; });
+    props.setLowpassCfg(newCfg);
+  };
+
+  const taps = props.cfg.taps.map((tap, i) => {
+    return <div>
+      <input type="text" value={tap.pos} onInput={e => setPos(e, i)}></input>
+      <input type="range" min="1" max="99" value={Math.round(tap.weight * 100)} onInput={e => setWeight(e, i)} />
+    </div>;
+  });
+
+  return taps;
+}
+
 function App(props: AppProps): JSX.Element {
   const [connected, setConnected] = useState(true);
   const [drumVolume, setDrumVolume] = useState(100);
   const [lowpParam, setLowpParam] = useState(50);
+  const [cfg, setCfg] = useState<LowpassControlBlock>({ selfWeight: 0.5, taps: [{ pos: 1, weight: 0.5 }] });
   const wsco = useRef<WebSocketContainer | undefined>(undefined);
 
   function reconnect(wsc: WebSocketContainer) {
@@ -121,11 +151,20 @@ function App(props: AppProps): JSX.Element {
     setLowpParam(lowp_param);
   };
 
+  function setLowpassCfg(cfg: LowpassControlBlock): void {
+    const selfWeight = 1 - cfg.taps.map(x => x.weight).reduce((a, b) => a + b);
+    const lowp_cfg = produce(cfg, cfg => {
+      cfg.selfWeight = selfWeight;
+    });
+    setCfg(cfg);
+    send({ message: { t: 'setLowpassConfig', lowp_cfg } });
+  }
+
   return <div>
     <button disabled={!connected} onMouseDown={() => { send({ message: { t: 'drum' } }) }}>Action</button><br />
     <button disabled={!connected} onMouseDown={() => { send({ message: { t: 'quit' } }) }}>Quit</button><br />
     <input disabled={!connected} type="range" min="0" max="100" value={drumVolume} onInput={drumVolOnInput} />
-    <input disabled={!connected} type="range" min="1" max="99" value={lowpParam} onInput={lowpParamOnInput} />
+    <LowpassCfg cfg={cfg} setLowpassCfg={setLowpassCfg} />
     {!connected ? <span><br /><button style={{ backgroundColor: 'red', color: 'white' }}
       onClick={() => { reconnect(wsco.current!); }}>reconnect</button></span> : undefined}
     <Sequencer send={send} />
