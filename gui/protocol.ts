eac181fb4d09fa2d3c554c18cac36403e7cdd76f
Jason Reed <jcreed@gmail.com>
2024-05-11 12:19:43 -0400

    Add lowpass cfg to protocol

--- gui/protocol.ts
+++ gui/protocol.ts
@@ -4,11 +4,22 @@ export type UgenSpec =
   | { t: 'ugenGroup', dst: number }
   ;
 
+export type Tap = {
+  pos: number, // integer >0
+  weight: number, // float in [0,1], together with selfWeight maybe should add up to < 1
+}
+
+export type LowpassControlBlock = {
+  selfWeight: number,
+  taps: Tap[],
+}
+
 export type WebAction =
   | { t: 'quit' }
   | { t: 'drum' }
   | { t: 'setVolume', vol: number }
   | { t: 'setLowpassParam', lowp_param: number }
+  | { t: 'setLowpassConfig', lowp_cfg: LowpassControlBlock }
   | { t: 'setSequencer', inst: number, pat: number, on: boolean }
   | { t: 'reconfigure', specs: UgenSpec[] }
   ;
