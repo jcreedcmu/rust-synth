770d4a077d79a5accc8b18bee50437773aa7c3e6
Jason Reed <jcreed@gmail.com>
2024-05-16 17:59:58 -0400

    colorful meters

--- gui/db-meter.tsx
+++ gui/db-meter.tsx
@@ -1,3 +1,4 @@
+import { CSSProperties } from 'react';
 import ReactDOM from 'react-dom';
 
 export type DbProps = {
@@ -8,6 +9,23 @@ function dbOfLevel(x: number) {
   return x < 1e-10 ? '-infinity' : 20 * Math.log(x) / Math.log(10);
 }
 
+const PIX_PER_DB = 3;
 export function DbMeter(props: DbProps): JSX.Element {
-  return <span>{dbOfLevel(props.value)} dB</span>;
+  const labelStyle: CSSProperties = {
+    display: 'inline-block',
+    margin: 1,
+    width: 100,
+    height: 20,
+    whiteSpace: 'nowrap',
+    overflowX: 'hidden',
+  };
+  const barWidth = props.value < 1e-10 ? 0 : 200 + PIX_PER_DB * 20 * Math.log(props.value) / Math.log(10);
+  const divStyle: CSSProperties = {
+    display: 'inline-block',
+    border: '1px solid black',
+    background: "linear-gradient(90deg, rgba(73,185,51,1) 164px, rgba(251,255,0,1) 182px, rgba(255,0,0,1) 200px)",
+    margin: 1,
+    width: barWidth,
+  };
+  return <span><div style={labelStyle}>{dbOfLevel(props.value)} dB</div><div style={divStyle} >&nbsp;</div></span>;
 }
